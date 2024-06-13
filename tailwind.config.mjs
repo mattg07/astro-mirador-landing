ad5d1d4ed618576cad5e21af96d215c40767c430
mattg07 <matiasmgodoy00@gmail.co.com>
2024-06-13 17:45:14 +0200

    final touch

---
+++ tailwind.config.mjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'purple': '#3f3cbb',
+      'midnight': '#121063',
+      'metal': '#565584',
+      'tahiti': '#3ab7bf',
+      'silver': '#ecebff',
+      'bubble-gum': '#ff77e9',
+	'bermuda': '#78dcca',
+		'mcorange' : '#ff8000',
+    },
+		extend: {},
+	},
+	plugins: [],
+}
